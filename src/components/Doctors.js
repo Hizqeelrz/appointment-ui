db3da0afa0f9db24d983e5b1faac1a4e2d24171c
hizqeelrz <hizqeel1214@gmail.com>
2019-02-05 03:14:46 +0500

    parent component cleaned with new components added

---
+++ src/components/Doctors.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Doctors extends React.Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Doctors;
