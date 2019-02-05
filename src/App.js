dc0872f90730615a8e92c54e1d59c91d98c9a908
hizqeelrz <hizqeel1214@gmail.com>
2019-02-05 18:55:42 +0500

    Doctors data fetched

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Doctors from './components/Doctors';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Doctors/>
       </div>
     );
   }
--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class Doctors extends React.Component {
+
+    state = {
+        doctors: []
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:4000/api/doctors").then(doc => {
+
+            const doctors = doc.data.data;
+            console.log(doc.data);
+            this.setState({
+                doctors
+            });
+        })
+    }
+    
     render() {
         return(
             <div>
-                
-            </div>
+                {this.state.doctors.map(doctor => 
+                    <div key={doctor.id}>
+                        <li>{doctor.name}</li>
+                        <li>{doctor.age}</li>
+                        <li>{doctor.gender}</li>
+                    </div>
+                    )}
+            </div>    
         )
     }
 }
